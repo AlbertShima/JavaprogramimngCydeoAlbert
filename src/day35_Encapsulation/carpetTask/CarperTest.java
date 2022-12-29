9e627b3f539b35da9e48dea9f8ef5dd558341b26
Albert Shima <albertshima@gmail.com>
2022-12-29 16:32:22 +0100

    till day 45>

--- src/day35_Encapsulation/carpetTask/CarperTest.java
+++
@@ -1,9 +0,0 @@
-package day35_Encapsulation.carpetTask;
-
-public class CarperTest {
-    public static void main(String[] args) {
-        Carpet carpet = new Carpet();
-        carpet.setInfo(20, 10, 100, false);
-        System.out.println(carpet);
-    }
-}
